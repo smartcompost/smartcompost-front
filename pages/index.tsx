6503b9c63b47de8ea32b3707de8871790cfb753f
Karolina Zielonka <karolina.zielonka@deployed.pl>
2022-11-20 11:08:43 +0100

    main view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,43 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline color-red">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+    <div className="flex h-screen w-96 flex-col my-8">
+      <div className="flex m-5">
+        <h1 className="text-xl">Hello John!</h1>
+      </div>
+      <div className="flex justify-around w-full my-4 items-center">
+        <div className="item w-48 text-center p-3 text-md">
+          Waste composted (since January 2022)
+        </div>
 
-export default Home
+        <div className="item w-24 text-center p-3 text-3xl font-bold">
+          23,4kg
+        </div>
+      </div>
+      <div className="flex justify-around w-full my-4 items-center">
+        <div className="item w-32 text-center text-md">
+          <Image
+            className="m-auto"
+            src="/compost.svg"
+            alt="Compost Logo"
+            width={200}
+            height={200}
+            priority
+          />
+        </div>
+
+        <div className="item w-32 text-center p-1">
+          <p className="text-sm">Yearly goal</p>
+          <p className="text-lg font-bold">50kg</p>
+          <p className="text-sm">You’ve reached</p>
+          <p className="tex-lg font-bold">46%</p>
+          <p className="text-sm">of your yearly goal</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
